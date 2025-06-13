8675b584b2c2fdfa769a05761cf387fa5b8ad607
imamz <imambiladi27@gmail.com>
2025-06-13 07:41:33 +0700

    first commit

---
+++ quote.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+    "fmt"
+    "rsc.io/quote"
+)
+// func Hello() string {
+// 	return sampler.Hello()
+// }
+func main() {
+    fmt.Println(quote.Hello()) // Menampilkan kutipan "Hello"
+}
